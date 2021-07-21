ef7e7fd10a37b5d6650b2585a2e81539e327ac46
Avinash <Avinashkumarsinha007@gmail.com>
2021-07-21 11:15:13 +0530

    product component

---
+++ Frontend/src/components/Product/product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { CustomCard } from "../CardComponents/CustomCard";
+export const Product = () => {
+    return (
+        <>
+            <CustomCard type="secondary">
+                <h>Maruti</h>
+           </CustomCard>
+        </>
+   )
+}
