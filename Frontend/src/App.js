2b9ebd165bfa6f21774f8c1f0e7127762cf09d9a
PRASADBIRARI <prasadpbirari7@gmail.com>
2021-07-21 19:20:27 +0530

    Home Page Search Box

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,13 @@
+import CarouselBanner from "./Components/Dashboard/CarouselBanner";
+import CarouselBannerOpen from "./Components/Dashboard/CarouselBannerOpen";
 import LoggedInNavbar from "./Components/LoggedInNavbar";
 
 function App() {
   return (
     <div>
-     
+     <CarouselBanner/>
+     <hr/>
+     {/* <CarouselBannerOpen/> */}
     </div>
   );
 }
---
+++ Frontend/src/Components/Routes/Routes.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { Route, Switch } from "react-router-dom";
+// import Dashboard from "../Pages/Dashboard/Dashboard";
+// import Open from "../Pages/Dashboard/Open";
+
+// const Routes = () => {
+//   return (
+//     <div>
+//       <Switch>
+//         <Route exact path="/">
+//           <Dashboard />
+//         </Route>
+//         <Route exact path="/open">
+//           <Open />
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// };
+
+// export { Routes };
