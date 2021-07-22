29a039365009410183570b7d46f5f13c407046de
shubham rathod <31065956+shubham-rathod1@users.noreply.github.com>
2021-07-22 10:37:23 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-      {/* <Provider /> */}
       <App />
-      {/* <Provider /> */}
-    {/* </BrowserRouter> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
