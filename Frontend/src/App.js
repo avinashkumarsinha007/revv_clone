c422bab26583358d821f3ae6d324392478a439d1
KANDURI MANOHAR <manoharkanduri@gmail.com>
2021-07-23 10:00:16 +0530

    How it works

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,13 @@
 import LoggedInNavbar from "./Components/LoggedInNavbar";
+import { HowItWorks } from "./Components/MiddleSection/HowItWorks";
+import { RevvB} from "./Components/MiddleSection/RevvB";
+import { WhyRevv } from "./Components/MiddleSection/WhyRevv";
 
 function App() {
   return (
     <div>
-     
+    <HowItWorks/>
+    <WhyRevv/>
     </div>
   );
 }
--- Frontend/src/Components/MiddleSection/WhyRevv.jsx
+++ Frontend/src/Components/MiddleSection/WhyRevv.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import {RevvB} from './RevvB'
+import styles from './MiddleSection.module.css'
 function WhyRevv(){
     return(
         <div>
             <h3>Why Revv</h3>
+            <RevvB />
+
+
         </div>
     )
 }
