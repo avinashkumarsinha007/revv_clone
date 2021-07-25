1f382dbe20e1e510d7f4a327ea3bb7fbe689415d
PRASADBIRARI <prasadpbirari7@gmail.com>
2021-07-25 13:38:57 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,6 @@ env.config();
 
 const connect = () => {
     return mongoose.connect(`${process.env.MONGODB_LINK}`, {
-    // return mongoose.connect('mongodb://localhost:27017', {
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: true,
